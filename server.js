91d1e62cf9e5ae1e6ee9d210a15a3ae5b4dc94ad
Bruna Enos <bruna_bsm@icloud.com>
2017-02-13 18:04:56 -0600

    [add] first commit

---
+++ server.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(__dirname));
+
+app.listen(process.env.PORT || 3000)
+
